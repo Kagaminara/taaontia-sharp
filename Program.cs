c66086a016dfda8d5b60f161de6e26dd5e6f3016
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-20 17:40:23 +0100

    Added token not found handling

--- Program.cs
+++ Program.cs
@@ -21,18 +21,26 @@ namespace Discord_Bot
 
         public async Task MainAsync()
         {
-            _client = new DiscordSocketClient();
-
-            _client.Log += Log;
-
             //  You can assign your bot token to a string, and pass that in to connect.
             //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            //var token = "";
+            var token = "";
 
             // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
             // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            var token = File.ReadAllText("token.txt");
+            try
+            {
+                token = File.ReadAllText("token.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                await Log(new LogMessage(LogSeverity.Critical, "server", "Could not find token.txt. Please ensure the file is located in the same folder as 'Discord Bot.exe'"));
+                return;
+            }
             // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
