adf2c474e4f92ea2cf037c258db21af4a3bf139f
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-24 13:54:15 +0100

    Removed duplicate logginservice, implemented a bunch of log functions

--- TaaontiaCore/Services/LoggingService.cs
+++ TaaontiaCore/Services/LoggingService.cs
@@ -4,13 +4,29 @@ namespace TaaontiaCore.Services
 {
     public class LoggingService
     {
+        private ConsoleColor _defaultColor;
         public LoggingService(IServiceProvider services)
         {
+            _defaultColor = Console.ForegroundColor;
         }
 
         public void Log(string log)
         {
             Console.WriteLine(log);
+        }
+
+        public void Warn(string log)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[WARN] " + log);
+            Console.ForegroundColor = _defaultColor;
+        }
+
+        public void Error(string log)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[ERROR] " + log);
+            Console.ForegroundColor = _defaultColor;
         }
     }
 }
