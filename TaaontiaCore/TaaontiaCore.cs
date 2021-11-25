577950abbfb1d07f500f7d80f4cd6bbf11e2d58c
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-26 00:19:33 +0100

    Added GameService to TaaontiaCore

--- TaaontiaCore/TaaontiaCore.cs
+++ TaaontiaCore/TaaontiaCore.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
 using TaaontiaCore.Database;
+using TaaontiaCore.Events.Fight;
 using TaaontiaCore.Services;
 
 namespace TaaontiaCore
@@ -8,13 +8,13 @@ namespace TaaontiaCore
     public class TaaontiaCore
     {
         private readonly ServiceProvider _services;
+        private readonly GameService _game;
 
         public TaaontiaCore()
         {
             _services = ConfigureServices();
 
-            var game = _services.GetRequiredService<GameService>();
-
+            _game = _services.GetRequiredService<GameService>();
         }
 
         private ServiceProvider ConfigureServices()
@@ -26,6 +26,11 @@ namespace TaaontiaCore
 
             var serviceProvider = services.BuildServiceProvider();
             return serviceProvider;
+        }
+
+        public FightResult Fight(FightEvent fightEvent)
+        {
+            return _game.HandleFight(fightEvent);
         }
     }
 }
