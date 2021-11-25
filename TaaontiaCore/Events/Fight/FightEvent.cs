103a36669d45a5353af87a91319e0fc5fc03d86d
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-26 00:18:56 +0100

    Added FightEvents

---
+++ TaaontiaCore/Events/Fight/FightEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaaontiaCore.Events.Fight
+{
+    public class FightEvent
+    {
+        public Guid SkillId;
+        public Guid SourceId;
+        public Guid TargetId;
+    }
+}
