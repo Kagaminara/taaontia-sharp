8957ab604decb78cc401000c8c3cdd4bfdc13a34
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-26 00:18:48 +0100

    Added GameService

--- TaaontiaCore/Services/GameService.cs
+++ TaaontiaCore/Services/GameService.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using TaaontiaCore.Database;
+using TaaontiaCore.Events.Fight;
 
 namespace TaaontiaCore.Services
 {
     public class GameService
     {
         private readonly TaaontiaEntities _db;
-            private readonly LoggingService _logging;
+        private readonly LoggingService _logging;
 
         public GameService(IServiceProvider services)
         {
             _db = services.GetRequiredService<TaaontiaEntities>();
             _logging = services.GetRequiredService<LoggingService>();
+        }
+
+        public FightResult HandleFight(FightEvent fightEvent)
+        {
+            return new FightResult
+            {
+                Result = EFightResult.SUCCESS,
+
+            };
         }
 
         public void SayBye()
