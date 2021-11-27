ee838060e950e455f0e1744990161f38e6f49b28
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-27 23:27:46 +0100

    Added PlayerRemoteId migration

---
+++ TaaontiaCore/Migrations/20211127222349_PlayerRemoteId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaaontiaCore.Migrations
+{
+    public partial class PlayerRemoteId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "RemoteId",
+                table: "Player",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0ul);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemoteId",
+                table: "Player");
+        }
+    }
+}
