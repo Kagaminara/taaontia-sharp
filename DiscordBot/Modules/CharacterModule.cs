cd88887f32620d7a988eee561a7d11b5263a9f2f
Kagaminara Shikatsu <misterarkandias@gmail.com>
2021-11-30 01:47:54 +0100

    Removed old database context from commands (commented most of code)

--- DiscordBot/Modules/CharacterModule.cs
+++ DiscordBot/Modules/CharacterModule.cs
@@ -6,30 +6,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaaontiaCore.Events;
 
 namespace Discord_Bot.Modules
 {
     [Name("Character")]
     [Summary("Character related commands")]
-    public class CharacterModule: ModuleBase<SocketCommandContext>
+    public class CharacterModule : ModuleBase<SocketCommandContext>
     {
-        private readonly DiscordBotEntities _db;
+        private readonly TaaontiaCore.TaaontiaCore _game;
 
         public CharacterModule(IServiceProvider services)
         {
-            _db = services.GetRequiredService<DiscordBotEntities>();
+            _game = services.GetRequiredService<TaaontiaCore.TaaontiaCore>();
         }
 
-        [Command("heal")]
-        [Summary("Heal yourself")]
-        public async Task HealAsync()
-        {
-            Character character = await _db.FindOrCreateConnectedCharacter(Context.User);
+        //[Command("heal")]
+        //[Summary("Heal yourself")]
+        //public async Task HealAsync()
+        //{
+        //    Character character = await _db.FindOrCreateConnectedCharacter(Context.User);
 
-            int hpHealed = character.MaxHealth - character.Health;
-            character.Health = character.MaxHealth;
-            await _db.SaveChangesAsync();
-            await ReplyAsync($"You healed yourself for {hpHealed}, you are now at full HP !");
+        //    int hpHealed = character.MaxHealth - character.Health;
+        //    character.Health = character.MaxHealth;
+        //    await _db.SaveChangesAsync();
+        //    await ReplyAsync($"You healed yourself for {hpHealed}, you are now at full HP !");
+        //}
+
+        [Command("register")]
+        [Summary("Create your character")]
+        public async Task RegisterAsync()
+        {
+            var result = await _game.Character.CreateCharacter(new NewCharacter
+            {
+                Name = Context.User.Username,
+                RemoteId = Context.User.Id,
+            });
+
+            if (result.Result == TaaontiaCore.Enums.EResult.FAILURE)
+            {
+                switch (result.Error)
+                {
+                    case TaaontiaCore.Enums.ECharacterCreationError.REMOTE_ID_ALREADY_EXISTS:
+                        await ReplyAsync("Your character already exist !");
+                        return;
+                    case TaaontiaCore.Enums.ECharacterCreationError.UNKNOWN_ERROR:
+                    default:
+                        await ReplyAsync("There was an error, but I don't know what exactly :/");
+                        return;
+                }
+            }
+            await ReplyAsync($"Your character named {Context.User.Username} has been created !");
         }
     }
 }
--- DiscordBot/Modules/FiendTypeModule.cs
+++ DiscordBot/Modules/FiendTypeModule.cs
@@ -15,124 +15,122 @@ namespace Discord_Bot.Modules
     [Group("fiendType")]
     public class FiendTypeModule : ModuleBase<SocketCommandContext>
     {
-        private readonly DiscordBotEntities _db;
         private readonly DiscordSocketClient _client;
         private readonly CommandService _commandService;
 
         public FiendTypeModule(IServiceProvider services)
         {
-            _db = services.GetRequiredService<DiscordBotEntities>();
             _client = services.GetRequiredService<DiscordSocketClient>();
             _commandService = services.GetRequiredService<CommandService>();
 
         }
 
-        [Command("add")]
-        [Summary("Add a fiend type to the fiend list")]
-        public async Task AddFiendAsync(string name, string description = "", int baseHealth = 0, int baseEnergy = 0)
-        {
-            if (String.IsNullOrWhiteSpace(name))
-            {
-                await ReplyAsync("You have to specify at least a name for you foe.\n`!addFiend name \"description\" baseHealth baseEnergy`");
-            }
-            else
-            {
-                string trimedName = name.Trim();
-                int existingFiendCount = _db.FiendType.Count(fiend => fiend.Name == trimedName);
+        //[Command("add")]
+        //[Summary("Add a fiend type to the fiend list")]
+        //public async Task AddFiendAsync(string name, string description = "", int baseHealth = 0, int baseEnergy = 0)
+        //{
+        //    if (String.IsNullOrWhiteSpace(name))
+        //    {
+        //        await ReplyAsync("You have to specify at least a name for you foe.\n`!addFiend name \"description\" baseHealth baseEnergy`");
+        //    }
+        //    else
+        //    {
+        //        string trimedName = name.Trim();
+        //        int existingFiendCount = _db.FiendType.Count(fiend => fiend.Name == trimedName);
 
-                if (existingFiendCount > 0)
-                {
-                    await ReplyAsync("A for with this name already exists.");
-                }
-                else
-                {
-                    var sb = new StringBuilder();
-                    var embed = new EmbedBuilder();
+        //        if (existingFiendCount > 0)
+        //        {
+        //            await ReplyAsync("A for with this name already exists.");
+        //        }
+        //        else
+        //        {
+        //            var sb = new StringBuilder();
+        //            var embed = new EmbedBuilder();
 
-                    await _db.AddAsync(new Database.FiendType
-                    {
-                        Name = name,
-                        Description = description,
-                        BaseHealth = baseHealth,
-                        BaseEnergy = baseEnergy,
-                    });
+        //            await _db.AddAsync(new Database.FiendType
+        //            {
+        //                Name = name,
+        //                Description = description,
+        //                BaseHealth = baseHealth,
+        //                BaseEnergy = baseEnergy,
+        //            });
 
-                    await _db.SaveChangesAsync();
+        //            await _db.SaveChangesAsync();
 
-                    sb.AppendLine(description);
-                    sb.AppendLine($"HP: {baseHealth} - SP: {baseEnergy}");
+        //            sb.AppendLine(description);
+        //            sb.AppendLine($"HP: {baseHealth} - SP: {baseEnergy}");
 
-                    embed.Title = name;
-                    embed.Description = sb.ToString();
+        //            embed.Title = name;
+        //            embed.Description = sb.ToString();
 
-                    await ReplyAsync(null, false, embed.Build());
-                }
-            }
-        }
+        //            await ReplyAsync(null, false, embed.Build());
+        //        }
+        //    }
+        //}
 
-        [Command("edit")]
-        [Summary("edit an existing fiend type")]
-        public async Task EditFiendAsync(long id, Database.FiendType fiendType)
-        {
+        //[Command("edit")]
+        //[Summary("edit an existing fiend type")]
+        //public async Task EditFiendAsync(long id, Database.FiendType fiendType)
+        //{
 
-            fiendType.Name = String.IsNullOrWhiteSpace(fiendType.Name) ? "" : fiendType.Name.Trim();
-            int existingFiendCount = _db.FiendType.Count(fiend => fiend.Name == fiendType.Name && fiend.Id != id);
+        //    fiendType.Name = String.IsNullOrWhiteSpace(fiendType.Name) ? "" : fiendType.Name.Trim();
+        //    int existingFiendCount = _db.FiendType.Count(fiend => fiend.Name == fiendType.Name && fiend.Id != id);
 
-            if (existingFiendCount > 0)
-            {
-                await ReplyAsync("A for with this name already exists.");
-                return;
-            }
-            var sb = new StringBuilder();
-            var embed = new EmbedBuilder();
+        //    if (existingFiendCount > 0)
+        //    {
+        //        await ReplyAsync("A for with this name already exists.");
+        //        return;
+        //    }
+        //    var sb = new StringBuilder();
+        //    var embed = new EmbedBuilder();
 
-            var result = _db.FiendType.SingleOrDefault(type => type.Id == id);
-            if (result != null)
-            {
-                result.Name = String.IsNullOrWhiteSpace(fiendType.Name) ? result.Name : fiendType.Name;
-                result.Description = String.IsNullOrWhiteSpace(fiendType.Description) ? result.Description : fiendType.Description;
-                result.BaseHealth = result.BaseHealth > 0 ? result.BaseHealth : fiendType.BaseHealth;
-                result.BaseEnergy = result.BaseEnergy > 0 ? result.BaseEnergy : fiendType.BaseEnergy;
-                await _db.SaveChangesAsync();
+        //    var result = _db.FiendType.SingleOrDefault(type => type.Id == id);
+        //    if (result != null)
+        //    {
+        //        result.Name = String.IsNullOrWhiteSpace(fiendType.Name) ? result.Name : fiendType.Name;
+        //        result.Description = String.IsNullOrWhiteSpace(fiendType.Description) ? result.Description : fiendType.Description;
+        //        result.BaseHealth = result.BaseHealth > 0 ? result.BaseHealth : fiendType.BaseHealth;
+        //        result.BaseEnergy = result.BaseEnergy > 0 ? result.BaseEnergy : fiendType.BaseEnergy;
+        //        await _db.SaveChangesAsync();
 
-                sb.AppendLine($"{result.Id}: {result.Name}");
-                sb.AppendLine($"Description: {result.Description}");
-                sb.AppendLine($"Base Health: {result.BaseHealth}");
-                sb.AppendLine($"Base Energy: {result.BaseEnergy}");
+        //        sb.AppendLine($"{result.Id}: {result.Name}");
+        //        sb.AppendLine($"Description: {result.Description}");
+        //        sb.AppendLine($"Base Health: {result.BaseHealth}");
+        //        sb.AppendLine($"Base Energy: {result.BaseEnergy}");
 
-                embed.Title = "Fiend successfully updated:";
-                embed.Color = new Color(0, 0, 255);
-                embed.Description = sb.ToString();
+        //        embed.Title = "Fiend successfully updated:";
+        //        embed.Color = new Color(0, 0, 255);
+        //        embed.Description = sb.ToString();
 
-                await ReplyAsync(null, false, embed.Build());
-                return;
-            }
-            await ReplyAsync($"Could not find the fiend with Id {id}.");
+        //        await ReplyAsync(null, false, embed.Build());
+        //        return;
+        //    }
+        //    await ReplyAsync($"Could not find the fiend with Id {id}.");
 
-        }
+        //}
 
-        [Command("list")]
-        [Summary("List all registered fiend types")]
-        public async Task ListFiendsAsync()
-        {
-            var sb = new StringBuilder();
-            var embed = new EmbedBuilder();
-            var fiendTypes = await _db.FiendType.ToListAsync();
+        //[Command("list")]
+        //[Summary("List all registered fiend types")]
+        //public async Task ListFiendsAsync()
+        //{
+        //    var sb = new StringBuilder();
+        //    var embed = new EmbedBuilder();
+        //    var fiendTypes = await _db.FiendType.ToListAsync();
 
-            foreach (var type in fiendTypes)
-            {
-                sb.AppendLine($"{type.Id}: {type.Name}");
-                sb.AppendLine($"Description: {type.Description}");
-                sb.AppendLine($"Base Health: {type.BaseHealth}");
-                sb.AppendLine($"Base Energy: {type.BaseEnergy}");
-                sb.AppendLine("--");
-            }
+        //    foreach (var type in fiendTypes)
+        //    {
+        //        sb.AppendLine($"{type.Id}: {type.Name}");
+        //        sb.AppendLine($"Description: {type.Description}");
+        //        sb.AppendLine($"Base Health: {type.BaseHealth}");
+        //        sb.AppendLine($"Base Energy: {type.BaseEnergy}");
+        //        sb.AppendLine("--");
+        //    }
 
-            embed.Title = "List of fiend types:";
-            embed.Color = new Color(0, 255, 0);
-            embed.Description = sb.ToString();
+        //    embed.Title = "List of fiend types:";
+        //    embed.Color = new Color(0, 255, 0);
+        //    embed.Description = sb.ToString();
 
-            await ReplyAsync(null, false, embed.Build());
-        }
+        //    await ReplyAsync(null, false, embed.Build());
+        //}
     }
 }
